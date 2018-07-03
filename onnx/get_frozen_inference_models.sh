e361105ce9c4e5d60403fc90f96aba9bf7d9eb93
Peng Wang (SIMPENG) <pengwa@microsoft.com>
2018-07-03 20:43:49 +0800

    make shell script more generic

--- onnx/get_frozen_inference_models.sh
+++ onnx/get_frozen_inference_models.sh
@@ -1,11 +1,22 @@
 #/bin/bash
 
-model_git_path="/home/pengwa/community/models/research/slim"
-tf_git_path="/home/pengwa/community/tensorflow"
+cur_path=`pwd`
+
+home=/home/pengwang
+model_git_path="$home/community/models/research/slim"
+tf_git_path="$home/community/tensorflow"
 
 model_name="resnet_v1_152"
 checkpoint_host="http://download.tensorflow.org/models/"
 checkpoint_link="resnet_v1_152_2016_08_28.tar.gz"
+
+ckt_path=/tmp/checkpoints
+exported_inf_path=/tmp/exported
+frozen_inf_path=/tmp/frozen
+
+mkdir $ckt_path
+mkdir $exported_inf_path
+mkdir $frozen_inf_path
 
 echo current working directory is `pwd`
 cpk_file=$checkpoint_host$checkpoint_link
@@ -13,14 +24,14 @@ echo downloading checkpoint file from $cpk_file
 wget $cpk_file
 echo extract checkpoint file, and delete the tar.gz file
 tar -xvf $checkpoint_link
-echo move checkpoint file to /tmp/checkpoints
-mv *.ckpt /tmp/checkpoints/$model_name
+echo move checkpoint file to $ckt_path
+mv *.ckpt $ckt_path/$model_name
 rm $checkpoint_link
 
 echo exporting inference graph 
 inference_graph_name=$model_name"_inf_graph.pb"
 echo change working directory to $model_git_path
-output_file_path=/tmp/exported/$inference_graph_name
+output_file_path=$exported_inf_path/$inference_graph_name
 echo export inference graph to $output_file_path
 cd $model_git_path
 python3 export_inference_graph.py  --alsologtostderr --model_name=$model_name --output_file=$output_file_path --labels_offset=1
@@ -31,19 +42,20 @@ cd $tf_git_path
 echo get the outputs name with summarize tool
 bazel build tensorflow/tools/graph_transforms:summarize_graph
 
-summarize_cmd="bazel-bin/tensorflow/tools/graph_transforms/summarize_graph --in_graph=/tmp/exported/$inference_graph_name "
+summarize_cmd="bazel-bin/tensorflow/tools/graph_transforms/summarize_graph --in_graph=$exported_inf_path/$inference_graph_name "
 echo $summarize_cmd
 outputs=`$summarize_cmd 2> /dev/null | grep -E "\-\-output_layer=" | sed -r 's/^.*--output_layer=(.*$)/\1/g'`
 echo output_node_name is $outputs
 
 echo freeze the graph based on downloaded checkpoint file
 bazel build tensorflow/python/tools:freeze_graph
-#update_output_name=`python3 /home/pengwa/community/update_output_name.py $outputs  2> /dev/null`
-#echo updated output name is $update_output_name
-freeze_cmd="bazel-bin/tensorflow/python/tools/freeze_graph --input_graph=$output_file_path --input_checkpoint=/tmp/checkpoints/$model_name --input_binary=true --output_graph=/tmp/frozen/$inference_graph_name --output_node_names=$outputs"
+freeze_cmd="bazel-bin/tensorflow/python/tools/freeze_graph --input_graph=$output_file_path --input_checkpoint=$ckt_path/$model_name --input_binary=true --output_graph=$frozen_inf_path/$inference_graph_name --output_node_names=$outputs"
 echo $freeze_cmd 
 
 result=`$freeze_cmd`
 
 
+echo "all frozen fraph is stored at " $frozen_inf_path
 
+echo switch back to original directory: $cur_path
+cd $cur_path
--- onnx/summarize_frozen_graph.sh
+++ onnx/summarize_frozen_graph.sh
@@ -1,6 +1,10 @@
 #/bin/bash
 
-tf_git_path="/home/pengwa/community/tensorflow"
+cur_path=`pwd`
+home="/home/pengwang"
+rename_script_path="$home/community/learning/onnx/update_name_with_index.py "
+tf_git_path="$home/community/tensorflow"
+tf_onnx_root="$home/community/tensorflow-onnx"
 
 echo change working directory to $tf_git_path
 cd $tf_git_path
@@ -9,15 +13,23 @@ bazel build tensorflow/tools/graph_transforms:summarize_graph
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
   echo -------------------- start handling $line ----------------------------
+  echo change working directory to $tf_git_path
+  cd $tf_git_path
   summarize_cmd="bazel-bin/tensorflow/tools/graph_transforms/summarize_graph --in_graph=/tmp/frozen/$line"
   echo $summarize_cmd
   bazel-bin/tensorflow/tools/graph_transforms/summarize_graph --in_graph=/tmp/frozen/$line
   outputs=`$summarize_cmd 2> /dev/null | grep -E "\-\-output_layer=" | sed -r 's/^.*--output_layer=(.*$)/\1/g'`
   inputs=`$summarize_cmd 2> /dev/null | grep -E "\-\-input_layer=" | sed -r 's/^.*--input_layer=(.*)[[:space:]]--input_layer_type.*$/\1/g'`
-  update_output_name=`python3 /home/pengwa/community/update_output_name.py $outputs  2> /dev/null`
-  update_input_name=`python3 /home/pengwa/community/update_output_name.py $inputs  2> /dev/null`
+  update_output_name=`python3 $rename_script_path $outputs  2> /dev/null`
+  update_input_name=`python3 $rename_script_path $inputs  2> /dev/null`
+  echo python3 $rename_script_path $inputs  2> /dev/null
   echo updated input names is $update_input_name, output names is $update_output_name
   echo start convertion
-  echo python3 -m tfonnx.convert --input "/tmp/frozen/$line" --inputs $update_input_name --outputs $update_output_name --output $line.onnx --verbose
-  python3 -m tfonnx.convert --input "/tmp/frozen/$line" --inputs $update_input_name --outputs $update_output_name --output $line.onnx --verbose
+  echo change working directory to $tf_onnx_root
+  cd $tf_onnx_root
+  python3 -m tf2onnx.convert --input "/tmp/frozen/$line" --inputs $update_input_name --outputs $update_output_name --output $line.onnx --verbose
+  echo python3 -m tf2onnx.convert --input "/tmp/frozen/$line" --inputs $update_input_name --outputs $update_output_name --output $line.onnx --verbose
 done < /tmp/temp_frozen.txt
+
+echo switch back to original directory: $cur_path
+cd $cur_path
