ffea4e24f0c7b4ecd552ccdec4c52ee71ba49362
Peng Wang (SIMPENG) <pengwa@microsoft.com>
2018-07-19 13:46:14 +0800

    fix update_name script

--- onnx/update_name_with_index.py
+++ onnx/update_name_with_index.py
@@ -3,4 +3,6 @@
 import sys
 
 
-print(','.join([a + ":"+ str(index) for index,a in enumerate(sys.argv[1].split(","))]))
+#print(','.join([a + ":"+ str(index) for index,a in enumerate(sys.argv[1].split(","))]))
+print(','.join([a + ":0" for index,a in enumerate(sys.argv[1].split(","))]))
+
